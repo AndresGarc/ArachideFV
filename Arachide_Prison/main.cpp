76415e89b9fa94e7aa9926eac48192a1b01882c8
Andres <andres.garciamiras@gmail.com>
2020-03-12 18:55:57 +0100

    Proyecto Arachide_Prison

---
+++ Arachide_Prison/main.cpp
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+
+int main() {
+
+  //BUCLE DEL JUEGO
+    //al tratarse de ser el contexto -> este debe ser ambiguo
+    // AL ESTADO -> tiene que controlar el estado en el que esta
+    // dependiendo del estado va a realizar el input / update / render
+    
+    //MODIFICAR EL MAIN AL GUSTO, CUANDO TENGAMOS LA MAQUINA DE ESTADOS TOCARÁ MODIFICAR
+    
+  return 0;
+}
